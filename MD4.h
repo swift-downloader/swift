92b226ad858fd0edee66c549a34b883da3c424d8
Mike Ma <zhtx10@gmail.com>
2011-07-31 23:18:47 +0800

    fixed building bugs of MD4.*

--- MD4.h
+++ MD4.h
@@ -1,8 +1,11 @@
 #ifndef MD4_H
 #define MD4_H
 
-typedef wchar_t uchar;
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef char uchar;
 
 uchar *auth_md4Sum( uchar *dst, const uchar *src, const int srclen );
 
-#endif MD4_H
+#endif
